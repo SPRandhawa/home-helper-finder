7aa09bbf6b29ee5f702600173f12a85d1152d3e9
Your Name <your-email@gmail.com>
2026-08-26 20:55:27 +0530

    commit

--- core/settings.py
+++ core/settings.py
@@ -125,16 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-<<<<<<< HEAD
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-=======
->>>>>>> 354ceeb6b916d0fc634aa01457899253005823db
-
-STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
